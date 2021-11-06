99b00fb9cffcfbe0928c7e86cef100870d7f5bce
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-11-06 21:51:41 +0700

    improv

--- extract.py
+++ extract.py
@@ -7,6 +7,12 @@ import zipfile
 import tarfile
 import subprocess
 import shutil
+import re
+
+def UNTAR(INPUT, OUTPUT):
+	tar = tarfile.open(INPUT)
+	tar.extractall(OUTPUT)
+	tar.close()
 
 def UNZIP(INPUT1, INPUT2):
 	with zipfile.ZipFile(INPUT1 , 'r') as zip_ref:
@@ -32,6 +38,7 @@ for LIST_INPUT in os.listdir(INPUT):
          	shutil.rmtree(OUTPUT + LIST_INPUT)
          	os.makedirs(OUTPUT + LIST_INPUT)	
          if LIST_INPUT.startswith("open_gapps"):
+         	print(" ")
          	print("- Opengapps Detected") 
          	print("- Extracting : ", INPUT + LIST_INPUT)
          	UNZIP(INPUT + LIST_INPUT, TMP + LIST_INPUT)
@@ -55,14 +62,59 @@ for LIST_INPUT in os.listdir(INPUT):
          				input_mv = os.path.join(root, FILES)
          				out_mv = os.path.join(OUTPUT, LIST_INPUT,  FILES)
          				shutil.copytree (input_mv, out_mv, dirs_exist_ok=True)	
-         			elif FILES == '480':
+         			elif FILES == '480' or FILES == '213-240' or FILES == '560-640':
          				input_mv = os.path.join(root, FILES)
          				out_mv = os.path.join(OUTPUT, LIST_INPUT,  FILES)
          				shutil.copytree (input_mv, out_mv, dirs_exist_ok=True)
-         			elif FILES == '320':
+         			elif FILES == '320' or FILES == '160' or FILES == '400-420-480':
          				input_mv = os.path.join(root, FILES)
          				out_mv = os.path.join(OUTPUT, LIST_INPUT,  FILES)
          				shutil.copytree (input_mv, out_mv, dirs_exist_ok=True)	
+         elif "LiteGapps" in LIST_INPUT:
+         	print(" ")
+         	print("- LiteGapps Detected") 
+         	print("- Extracting : ", INPUT + LIST_INPUT)
+         	UNZIP(INPUT + LIST_INPUT, TMP + LIST_INPUT)
+         	for root, dirs, files in os.walk(TMP + LIST_INPUT):
+         		for file in files:
+         			if file.endswith('.tar.xz'):
+         				print("- Extracting : ", file)
+         				UNTAR(os.path.join(root, file),OUTPUT + LIST_INPUT)
+         elif "FlameGApps" in LIST_INPUT:
+         	print(" ")
+         	print("- FlameGApps Detected") 
+         	print("- Extracting : ", INPUT + LIST_INPUT)
+         	UNZIP(INPUT + LIST_INPUT, TMP + LIST_INPUT)
+         	for root, dirs, files in os.walk(TMP + LIST_INPUT):
+         		for file in files:
+         			if file.endswith('.tar.xz'):
+         				print("- Extracting : ", file)
+         				UNTAR(os.path.join(root, file),OUTPUT + LIST_INPUT)
+         #
+         elif "BiTGApps" in LIST_INPUT:
+         	print(" ") 
+         	print("- BiTGApps Detected") 
+         	print("- Extracting : ", INPUT + LIST_INPUT)
+         	UNZIP(INPUT + LIST_INPUT, TMP + LIST_INPUT)
+         	for root, dirs, files in os.walk(TMP + LIST_INPUT):
+         		for file in files:
+         			if file.endswith('.tar.xz'):
+         				print("- Extracting : ", file)
+         				UNTAR(os.path.join(root, file), root)
+         				os.remove(os.path.join(root, file))
+         	shutil.copytree (TMP + LIST_INPUT, OUTPUT + LIST_INPUT , dirs_exist_ok=True) 
+         elif "NikGapps" in LIST_INPUT:
+         	print(" ") 
+         	print("- NikGapps Detected") 
+         	print("- Extracting : ", INPUT + LIST_INPUT)
+         	UNZIP(INPUT + LIST_INPUT, TMP + LIST_INPUT)
+         	for root, dirs, files in os.walk(TMP + LIST_INPUT):
+         		for file in files:
+         			if file.endswith('.tar.xz'):
+         				print("- Extracting : ", file)
+         				UNTAR(os.path.join(root, file), root)
+         				os.remove(os.path.join(root, file))
+         	shutil.copytree (TMP + LIST_INPUT, OUTPUT + LIST_INPUT , dirs_exist_ok=True) 
          else:
          	print("! Package not support : ", LIST_INPUT)
          	
@@ -70,4 +122,4 @@ for LIST_INPUT in os.listdir(INPUT):
         print("! Is Not package file : ", INPUT + LIST_INPUT)
         
         
-shutil.rmtree(TMP)
+#shutil.rmtree(TMP)
